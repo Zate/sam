24ce1e331601cc0ca2c0d582876003a758c6eb49
Zate <zate75@gmail.com>
2020-10-21 01:43:46 -0500

    fixing

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
 	"os"
-	"path"
 
-	"github.com/PuerkitoBio/goquery"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/publicsuffix"
 )
@@ -37,128 +33,6 @@ var SplunkAuthURL = "https://account.splunk.com/api/v1/okta/auth"
 // https://splunkbase.splunk.com/api/v1/app/2919
 // might need to crawl https://splunkbase.splunk.com/app/2919 to grab version info
 //https://docs.splunk.com/Documentation/SplunkbaseAPI/current/SBAPI/About
-
-// GetApp will download this appid from splunkbase
-func GetApp(aid string) string {
-	cr := LoadCreds()
-	options := cookiejar.Options{
-		PublicSuffixList: publicsuffix.List,
-	}
-	jar, err := cookiejar.New(&options)
-	if err != nil {
-		log.Fatal(err)
-	}
-	a := &http.Client{Jar: jar}
-	res, err := a.PostForm(SplunkAuthURL, url.Values{
-		"password": {cr.password},
-		"username": {cr.username},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	u, err := url.Parse(SplunkAuthURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, cookie := range jar.Cookies(u) {
-		fmt.Printf("  %s: %s\n", cookie.Name, cookie.Value)
-	}
-
-	log.WithFields(log.Fields{
-		"func":    "GetApp",
-		"app_id":  aid,
-		"cookies": res.Cookies(),
-	}).Print("Cookies After Auth")
-
-	surl := "https://splunkbase.splunk.com/app/" + aid
-	// Request the HTML page.
-
-	res, err = a.Get(surl)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// bdy, err := ioutil.ReadAll(res.Body)
-	// fmt.Print(string(bdy))
-
-	defer res.Body.Close()
-
-	u, err = url.Parse(surl)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, cookie := range jar.Cookies(u) {
-		fmt.Printf("  %s: %s\n", cookie.Name, cookie.Value)
-	}
-	log.WithFields(log.Fields{
-		"func":         "GetApp",
-		"app_id":       aid,
-		"cookies":      res.Cookies(),
-		"sent_cookies": res.Request.Cookies(),
-	}).Print("Cookies After Get Splunkbase URL")
-
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// <sb-release-select-click u-for="download-modal" sb-selector="release-version" sb-target="5.4.0" class="u.hide@sm u.item:6/12@xl u.item:1/1@lg u.item:1/1@md u.btn:green" sb-href="/app/2919/release/5.4.0/download/" data-ol-has-click-handler="">
-	//                                                 Download
-	//                                             </sb-release-select-click>
-
-	// Find the review items
-	doc.Find("select").Each(func(i int, s *goquery.Selection) {
-		// log.Print(i)
-		id, ex := s.Attr("id")
-		if ex && id == "release-version" {
-			ver, _ := s.Find("option").First().Attr("value")
-			// log.Print(ver)
-			dlurl := surl + "/release/" + ver + "/download/"
-			// log.Print(dlurl)
-			res, err = a.Get(dlurl)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer res.Body.Close()
-			u, err = url.Parse(dlurl)
-			if err != nil {
-				log.Fatal(err)
-			}
-			for _, cookie := range jar.Cookies(u) {
-				fmt.Printf("  %s: %s\n", cookie.Name, cookie.Value)
-			}
-			log.WithFields(log.Fields{
-				"func":             "GetApp",
-				"app_id":           aid,
-				"cookies":          res.Cookies(),
-				"sent_cookies":     res.Request.Cookies(),
-				"Response_Headers": res.Header,
-			}).Print("Cookies After Download")
-
-			out, err := os.Create(path.Base(res.Request.URL.String()))
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer out.Close()
-
-			// Write the body to file
-			if res.StatusCode != 200 {
-				log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-			}
-			_, err = io.Copy(out, res.Body)
-
-		}
-		// fmt.Println(i, title)
-
-	})
-
-	return aid
-
-}
 
 // SplunkAuth takes a set of creds, and auths to Splunk Okta endpoint, returns Auth struct
 func SplunkAuth() (client http.Client) {
@@ -223,4 +97,10 @@ func main() {
 	log.Print("Main Started")
 	log.Debug("Debug is on")
 	GetApp("2919")
+	// Get Addon
+	// Package App
+	// Package Addon
+	// Combine
+	// Deploy
+
 }
---
+++ sbase.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+source .env
+AUTH=`curl -sS -d "username=${SBASE_U}&password=${SBASE_P}" -X POST https://splunkbase.splunk.com/api/account:login/ | grep -o -P '(?<=<id>).*(?=</id>)'`
+VER=`curl -sS -k https://splunkbase.splunk.com/app/2890/ | grep -oP '(?<=<sb-release-select u-for="download-modal" sb-selector="release-version" sb-target=").*(?=" )' | head -1`
+APPURL="${1}/release/${VER}/download/"
+curl -vvv -k -u admin:stuff123 -d name=${APPURL} -d update=true -d filename=true -d auth=${AUTH} https://localhost:8089/services/apps/local/
+
+# ToDo
+# Mount etc/apps to local file system
+
+#https://splunkbase.splunk.com/api/v1/app/2890/release/
+
+# {
+# "id": 16431, <-- package_id ?
+# "app": 2890, <--- can start with this.
+# "name": "5.2.0", <-- get this
+# "release_notes": "For the latest release notes, see http://docs.splunk.com/Documentation/MLApp/latest/User/Whatsnew",
+# "CIM_versions": [],
+# "splunk_versions": [
+# 31,
+# 25,
+# 30
+# ],
+# "public": true,
+# "public_ever_true": true,
+# "created_datetime": "2020-05-09T00:07:29.055882Z",
+# "published_datetime": "2020-05-09T00:07:29.055918Z",
+# "size": 16524687,
+# "filename": "splunk-machine-learning-toolkit_520.tgz", <-- useful.
+# "platform": "independent",
+# "is_bundle": true,
+# "has_ui": false,
+# "approved": false,
+# "appinspect_status": false,
+# "install_method_single": "simple",
+# "install_method_distributed": "appmgmt_phase",
+# "requires_cloud_vetting": true,
+# "appinspect_request_id": null,
+# "cloud_vetting_request_id": "9841b444-9a82-4254-9f77-7ab73107ef36",
+# "python3_acceptance": true,
+# "python3_acceptance_datetime": "2020-05-15T04:00:45.552000Z",
+# "python3_acceptance_user": 235284,
+# "fedramp_validation": "no",
+# "cloud_compatible": true
+# },
+
+
+#https://splunkbase.splunk.com/api/v1/app/2890/
+# {
+# "uid": 2890,
+# "appid": "Splunk_ML_Toolkit",
+# "title": "Splunk Machine Learning Toolkit",
+# "created_time": "2015-09-15T15:40:42+00:00",
+# "published_time": "2015-09-15T15:40:42+00:00",
+# "updated_time": "2020-05-09T00:07:29+00:00",
+# "license_name": "Splunk Software License Agreement",
+# "type": "app",
+# "license_url": "http://www.splunk.com/en_us/legal/splunk-software-license-agreement.html",
+# "description": "Splunk Machine Learning Toolkit\n\nThe Splunk Machine Learning Toolkit App delivers new SPL commands, custom visualizations, assistants, and examples to explore a variety of ml concepts. \n\nEach assistant includes end-to-end examples with datasets, plus the ability to apply the visualizations and SPL commands to your own data. You can inspect the assistant panels and underlying code to see how it all works.\n\nML Youtube Playlist http://tiny.cc/splunkmlvideos\nML Cheat Sheet http://tiny.cc/mltkcheatsheet\n\nAssistants:\n* Predict Numeric Fields (Linear Regression): e.g. predict median house values.\n* Predict Categorical Fields (Logistic Regression): e.g. predict customer churn.\n* Detect Numeric Outliers (distribution statistics): e.g. detect outliers in IT Ops data.\n* Detect Categorical Outliers (probabilistic measures): e.g. detect outliers in diabetes patient records.\n* Forecast Time Series: e.g. forecast data center growth and capacity planning.\n* Cluster Numeric Events: e.g. Cluster Hard Drives by SMART Metrics\n\nSmart Assistants (new assistants with revamped UI and better ml pipeline/experiment management):\n*Smart Forecasting Assistant (provides enhanced time-series analysis for users with little to no SPL knowledge and leverages the StateSpaceForecasting algorithm): e.g. forecasting app logons with special days\n\nAvailable on both on-premise and cloud.\n\nDeep Learning Toolkit for Splunk\nIntegrate with advanced custom machine learning systems using the Deep Learning Toolkit for Splunk (https://splunkbase.splunk.com/app/4607/). It extends Splunk’s Machine Learning Toolkit with prebuilt Docker containers for TensorFlow 2.0, PyTorch and a collection of NLP libraries. Python expertise is required to create your own neural networks.\nAvailable only for on-premise customers.\n\nSplunk Community for MLTK Algorithms on GitHub\nCheck out our Open Source community on Github that lets you share your algorithms with the community of Splunk MLTK users or import one of the algorithms that have been shared by the community: https://github.com/splunk/mltk-algo-contrib\n\nThe GitHub repo algorithms are also available as an app which provides access to custom algorithms. Cloud customers can use GitHub algorithms via this app and need to create a support ticket to have this installed:https://splunkbase.splunk.com/app/4403/\nAvailable on cloud and on-premise",
+# "access": "unrestricted",
+# "appinspect_passed": false,
+# "path": "https://splunkbase.splunk.com/app/2890/",
+# "install_method_distributed": "appmgmt_phase",
+# "install_method_single": "simple",
+# "download_count": 84203,
+# "install_count": 12333,
+# "archive_status": "live",
+# "is_archived": false,
+# "fedramp_validation": "no"
+# }
+
+# id is package id?
+# https://splunkbase.splunk.com/api/v1/app/2890/release/16431/
+# {
+# "app": 2890,
+# "name": "5.2.0",
+# "release_notes": "For the latest release notes, see http://docs.splunk.com/Documentation/MLApp/latest/User/Whatsnew",
+# "splunk_versions": [
+# "8.1",
+# "8.0",
+# "8.1.2008"
+# ],
+# "CIM_versions": [],
+# "public": true
+# }
